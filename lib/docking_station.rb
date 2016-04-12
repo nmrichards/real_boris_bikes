48733f42b481ee5740bba2ae4bbb940be43dca6b
MariaRomero <fantasysystems@gmail.com>
2016-04-12 16:31:14 +0100

    challenge 12 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,11 +2,12 @@
 
 class DockingStation
 	def release_bike
-    fail 'No bikes available' unless @bike
+    	fail 'No bikes available' unless @bike
 		@bike
 	end
 
 	def dock(bike)
+		fail 'Dock is full' if @bike
 		@bike = bike
 	end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,13 @@
 require "docking_station"
 require "bike"
 
-describe DockingStation do
-
-	it "responds to release_bike" do
-		expect(subject).to respond_to(:release_bike)
-	end
+describe DockingStation do	
 
 	it "working bike" do
 		new_bike = Bike.new
 		new_bike.working?.should be true
 	end
 
-	it "docks a bike" do
-		bike = Bike.new
-		expect(subject.dock(bike)).to eq bike
-	end
 
 	it "returns docked bikes" do
 		bike = Bike.new
@@ -23,7 +15,16 @@ describe DockingStation do
 		expect(subject.bike).to eq bike
 	end
 
+	it "docks a bike" do
+			bike = Bike.new
+			expect(subject.dock(bike)).to eq bike
+	end
+
 	describe '#release_bike' do
+
+		it "responds to release_bike" do
+		expect(subject).to respond_to(:release_bike)
+		end
 
 		it 'releases a bikes' do
 			bike = Bike.new
@@ -34,7 +35,17 @@ describe DockingStation do
 		it 'raises an error when no bikes are available' do
 			expect { subject.release_bike}.to raise_error 'No bikes available'
 		end
+
 	end
+
+
+
+	describe '#dock' do
+		it 'raises an error when dock is full' do 
+			subject.dock(Bike.new)
+			expect {subject.dock Bike.new}.to raise_error 'Dock is full'
+		end
+	end	
 
 
 
