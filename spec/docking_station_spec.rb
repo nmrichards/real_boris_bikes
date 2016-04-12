362b7ba1995e1ce25b1e92ccac8fb1f017cdf77a
Nick <nm.richards@yahoo.com>
2016-04-12 06:48:54 -0400

    Test spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,19 @@
 require "docking_station"
 require "bike"
 
-describe DockingStation do  	
-	it "responds to release_bike" do 
-		expect(subject).to respond_to(:release_bike) 
+describe DockingStation do
+	it "responds to release_bike" do
+		expect(subject).to respond_to(:release_bike)
 	end
 
 	it "working bike" do
 		new_bike = Bike.new
 		new_bike.working?.should be true
-	end	
+	end
+
+	#testing
 
 	# one liner syntax:
 	# it { is_expected.to respond_to :release_bike }
-		
+
 end
